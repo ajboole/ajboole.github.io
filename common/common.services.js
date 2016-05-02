87f01be8b6bd867708c691c93d743377ab71c63c
Schumacher <703354433@sanfordhealth.org>
2016-05-02 09:57:46 -0500

    Cleaned out unnecessary files

--- common/common.services.js
+++
@@ -1,8 +0,0 @@
-﻿(function () {
-	"use strict";
-	var app = angular.module("smhiApp", ["ngResource"])
-		.constant("appSettings",
-		{
-			serverPath: "http://students.cs.ndsu.nodak.edu/~jonwalke/API/Data/GetData.php?format=json"
-		});
-}());
